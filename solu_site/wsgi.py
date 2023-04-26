0c9cd13243cbc126f9ff58cfcb0952048e4808ee
iput2020-awaiishima <it00412hinata.awaiishima@gmail.com>
2023-04-26 11:34:15 +0900

    first commit

---
+++ solu_site/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for solu_site project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solu_site.settings')
+
+application = get_wsgi_application()
+app = application
